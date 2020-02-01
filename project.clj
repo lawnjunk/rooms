5222ec8af1e71b94d50b6d27f77ec1bdcc1cdf72
Duncan Marsh <slugbyte@slugbyte.com>
2020-02-01 11:04:44 +0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject rooms "0.1.0-SNAPSHOT"
+  :description "A simple multiplayer text game"
+  :url "https://www.github.com/slugbyte/rooms"
+  :license {:name "MIT"
+            :url "TODO"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot rooms.core
+  )
